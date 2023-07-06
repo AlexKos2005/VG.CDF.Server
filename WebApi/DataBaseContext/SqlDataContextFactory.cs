84bfc1ef2d1a20c90ceae41af245aa415c77012a
Полеченко Алексей Александрович <49471330+AlexKos2005@users.noreply.github.com>
2023-07-06 18:54:41 +0300

    feat:

--- WebApi/DataBaseContext/SqlDataContextFactory.cs
+++ WebApi/DataBaseContext/SqlDataContextFactory.cs
@@ -8,7 +8,9 @@ public class SqlDataContextFactory : IDesignTimeDbContextFactory<SqlDataContext>
     public SqlDataContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<SqlDataContext>();
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=volgor_asud_new;Username=postgres;Password=sa;");
+        optionsBuilder.UseNpgsql("Host=89.44.197.196;Port=5432;Database=volgor_asud;Username=postgres;Password=!PGadmin!;");
+        /*var optionsBuilder = new DbContextOptionsBuilder<SqlDataContext>();
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=volgor_asud_new;Username=postgres;Password=sa;");*/
 
         return new SqlDataContext(optionsBuilder.Options);
     }
